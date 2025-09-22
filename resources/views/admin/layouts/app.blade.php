b8baf1b089fc286303508edc92c92848c7b903b9
jofan-cah <jofan.fathurahman@gmail.com>
2025-09-22 10:37:41 +0700

    f

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -427,6 +427,10 @@
             currentX = null;
         });
     </script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+
 
     @stack('scripts')
 </body>
