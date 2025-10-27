d7b3c92a0c1688af9a6413a2d328c1538f0f0523
jofan-cah <jofan.fathurahman@gmail.com>
2025-10-27 16:13:56 +0700

    f

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -161,7 +161,8 @@ class Notification extends Model
             'type' => 'reminder_clock_in',
             'title' => 'Reminder Absen Masuk',
             // 'message' => "Jangan lupa absen masuk ya! Shift {$shiftName} sudah dimulai.",
-             'message' => "Wayahe Kerjo njir Absent e yo di pikir ! shift {$shiftName} sudah dimulai.",
+            //  'message' => "Wayahe Kerjo njir Absent e yo di pikir ! shift {$shiftName} sudah dimulai.",
+             'message' => "HIDUP JOK.......",
             'data' => [
                 'jadwal_id' => $jadwalId,
                 'date' => now()->format('Y-m-d')
